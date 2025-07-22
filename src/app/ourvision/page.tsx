e9de055aac7412980657ec13551e87956ff65bd1
prannykrish <pkrishnan0704@gmail.com>
2025-07-22 15:05:43 -0500

    please bruh

--- src/app/ourvision/page.tsx
+++ src/app/ourvision/page.tsx
@@ -5,10 +5,7 @@ import FooterSection from "@/components/footer";
 //import LawStatisticsSection from "@/components/lawstatistics";
 import { HeroHeader } from "@/components/nav-bar";
 import MissionVisionSection from "@/components/missionvision";
-import { OurVisionHeader } from "@/components/ourvisionheader";
-import { ProblemSection } from "@/components/problemsection";
-import { SolutionSection } from "@/components/solution";
-import  VisionSection  from "@/components/vision";
+
 import WhyBroken from "@/components/whybrokensystem";
 
 export default function OurVision() {
@@ -17,7 +14,7 @@ export default function OurVision() {
       <HeroHeader />
       <BrokenSystem/>
       <WhyBroken/>
-      {/* <VisionSection/> */}
+    
       <MissionVisionSection/>
       <FooterSection/>
    
